cd44c8299bf7c09bd59c03a92e75e970da3e1e15
lhzuin <luishenriquezuinruiz@gmail.com>
2022-07-07 16:04:55 -0300

    Continuação da implementação da escolha de datas

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from markupsafe import escape
 from flask_cors import CORS
 import user_db
+from datetime import date
 #from choose_room_form import pergunta
 app = Flask(__name__)
 CORS(app)
