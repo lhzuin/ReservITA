45e467fda3dfa920fc9a1c11fd20fbcd33dc148f
William Ryu Gyotoku <wrgyotoku@gmail.com>
2022-07-08 13:03:38 -0300

    funcao valid_login

--- user_db.py
+++ user_db.py
@@ -1,5 +1,31 @@
+from flask_wtf import FlaskForm
+import validators
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+from flask import render_template
+from app import app
+
+
+from app import LoginForm
+
+
+
 def valid_login(username, password):
-    return True
+
+    username = StringField('username', [validators.DataRequired()])
+    password = PasswordField('password', [validators.DataRequired()])
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+        return True
+    else:
+        print(form.errors)
+        return False    
+
+    return render_template('form.html', form=form)
 class User:
     def __init(self, name, phone, email, password, priority, status):
         self.name = name
