7201318da66d18a2ae0671b741c0050ceaa9eeee
bmfranzini <bruno.m.franzini@gmail.com>
2022-07-08 14:17:51 -0300

    Adding database methods

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import datetime
 from flask import Flask, render_template, url_for, request, redirect
 from markupsafe import escape
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 import room_db
 import user_db
@@ -16,6 +17,50 @@ username = None
 
 app = Flask(__name__)
 CORS(app)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:brunosql@localhost/reservita'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Horarios(db.Model):
+    __tablename__ = 'horarios'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(20))
+    room = db.Column(db.String(200))
+    intervalo = db.Column(db.String(200))
+    aluno = db.Column(db.String(200))
+
+    def __init__(self, date, room, intervalo, aluno):
+        self.date = date
+        self.room = room
+        self.intervalo = intervalo
+        self.aluno = aluno
+
+
+def check_schedule(room, date):
+    #room ex: "A - Churrasqueira"
+    #date ex: "08/07/2022"
+    horarios_livres = ["00:00 - 00:30", "00:30 - 01:00", "01:00 - 01:30", "01:30 - 02:00", "07:00 - 07:30", "07:30 - 08:00"]
+    for horario in horarios_livres:
+        if db.session.query(Horarios).filter(Horarios.intervalo == horario and Horarios.room == room and Horarios.date == date).count() != 0:
+            horarios_livres.remove(horario)
+    return horarios_livres # returns free schedule
+
+
+def make_reservation(room, date, time, user):
+    reservation = Horarios(date, room, time, user)
+    db.session.add(reservation)
+    db.session.commit()
+
 
 @app.route('/')
 def index():
@@ -65,7 +110,7 @@ def select_time():
         reservation_date = date.today() + datetime.timedelta(days = shift)
         global final_date
         final_date = str(reservation_date.day) + '/' + str(reservation_date.month) + '/' + str(reservation_date.year)
-        available_schedule = room_db.check_schedule(selected_room, final_date)
+        available_schedule = check_schedule(selected_room, final_date)
         return render_template('choose_hour_form.html', chosen_date = final_date, room = selected_room, schedule = available_schedule)
     else:
         return render_template('form.html', error=error)
